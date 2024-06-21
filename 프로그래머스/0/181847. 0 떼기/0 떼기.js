87390a1cf159b6cc5bb896a9b29691cfb2d0af3b
kimabraham <kimkr88@icloud.com>
2024-06-22 06:52:14 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,12 @@
+function solution(n_str) {
+    var answer = '';
+    let index = 0;
+    for(let i = 0; i < n_str.length; i++){
+        if(n_str[i] !== '0'){
+            index = i;
+            break;
+        }
+    }
+    
+    return n_str.slice(index);
+}
