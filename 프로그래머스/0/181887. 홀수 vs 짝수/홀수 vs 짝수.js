23931bd7b9b1b197b5989a321f278c04400324e4
kimabraham <kimkr88@icloud.com>
2024-06-22 06:56:52 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,5 @@
+function solution(num_list) {
+    const odd = num_list.reduce((acc, cur, i) => i % 2 === 0 ? acc + cur : acc, 0);
+    const even = num_list.reduce((acc, cur, i) => i % 2 ? acc + cur : acc, 0);
+    return odd >= even ? odd : even;
+}
