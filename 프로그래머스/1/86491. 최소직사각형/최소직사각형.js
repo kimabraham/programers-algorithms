2be74f3cd438d35a7d217150171952a9771db670
kimabraham <kimkr88@icloud.com>
2024-06-22 06:01:46 +0900

    [level 1] Title: 최소직사각형, Time: 29.29 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,20 @@
+function solution(sizes) {
+    const arr = sizes.map(size=>[size[0], size[1]]);
+    
+    for(size of arr){
+        if(size[0] < size[1]){
+            const width = size[0]
+            size[0] = size[1];
+            size[1] = width;
+            continue;
+        }
+    }
+    
+    const widths = arr.map(size=>size[0]);
+    const heights = arr.map(size=>size[1]);
+    
+    const maxWidth = Math.max(...widths);
+    const maxHeight = Math.max(...heights);
+    
+    return maxWidth * maxHeight;
+}
